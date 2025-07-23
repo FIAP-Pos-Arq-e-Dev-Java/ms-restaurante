393afe0623fb6e06206d4677333ee39afc5b98fc
Ghustavo516 <ghustavo516@gmail.com>
2025-07-22 22:27:02 -0300

    feat: correção de erro por conta de uuid

--- src/main/java/com/fiap/ms/restaurante/infraestrutura/dataproviders/database/entities/ItemCardapioEntity.java
+++ src/main/java/com/fiap/ms/restaurante/infraestrutura/dataproviders/database/entities/ItemCardapioEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -12,11 +13,13 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name="tb_item_cardapio")
+@Table(name = "tb_item_cardapio")
 public class ItemCardapioEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", updatable = false, nullable = false, columnDefinition = "BINARY(16)")
     private UUID id;
 
     private String nome;
--- src/main/java/com/fiap/ms/restaurante/infraestrutura/dataproviders/database/entities/RestauranteEntity.java
+++ src/main/java/com/fiap/ms/restaurante/infraestrutura/dataproviders/database/entities/RestauranteEntity.java
@@ -1,12 +1,30 @@
 package com.fiap.ms.restaurante.infraestrutura.dataproviders.database.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
 import java.util.UUID;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_restaurante")
 public class RestauranteEntity {
 
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", updatable = false, nullable = false, columnDefinition = "BINARY(16)")
     private UUID id;
+
     private String nome;
+
     private String endereco;
+
     private String horarioFuncionamento;
 
 //    private Usuario usuario
